46c0f97bfb842b575d86c9b42eca55cfa9d74201
Michael J Sigg <mjsigg@gmail.com>
2022-07-13 13:53:30 -0500

    yes

--- src/main/java/TestServlet.java
+++ src/main/java/TestServlet.java
@@ -6,14 +6,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "TestServlet", urlPatterns = "/test")
+@WebServlet(name = "TestServlet", urlPatterns = "/hello")
 public class TestServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String firstName = request.getParameter("name");
-        response.setContentType("text/html");
+
+        if(firstName == null) {
+            firstName = "bruh.";
+        }
+        
         PrintWriter out = response.getWriter();
-        firstName = ("Nobody. ");
+        response.setContentType("text/html");
+
+
         out.println("<h1> Hello " + firstName + "You are the real hero!</h1>");
         out.println("<img width=\"100\" height=\"200\" src=\"/Users/michaelsigg/IdeaProjects/coffee-project/img/will-smith-chris-rock.gif\">\n");
 
